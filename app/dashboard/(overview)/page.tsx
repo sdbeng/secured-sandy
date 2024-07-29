685b782942c94e5477fbc0c6e2a38c550d0905bb
Sergio D. Barrera <sdbeng@gmail.com>
2024-07-29 08:43:56 -0700

    using a route group to ensure loading.tsx only applies to your dashboard overview page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
-import { Card } from "../ui/dashboard/cards"
-import RevenueChart from "../ui/dashboard/revenue-chart"
-import LatestInvoices from "../ui/dashboard/latest-invoices"
-import { lusitana } from "../ui/fonts"
+import { Card } from "../../ui/dashboard/cards"
+import RevenueChart from "../../ui/dashboard/revenue-chart"
+import LatestInvoices from "../../ui/dashboard/latest-invoices"
+import { lusitana } from "../../ui/fonts"
 import { 
     fetchRevenue,
     fetchLatestInvoices,

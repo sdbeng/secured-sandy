39c465dc984f83363ff5f63fabd73b7bc14c816d
Sergio D. Barrera <sdbeng@gmail.com>
2024-07-31 08:22:15 -0700

    add debouncing, to reduce the number of requests sent to the database, saving resources

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import {useDebouncedCallback} from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  const handleSearch = (term: string) => {
-    console.log('term===', term);
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log('Searching term===', term);
     const params = new URLSearchParams(searchParams);
     if(term) {
       params.set('query', term);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
 
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">

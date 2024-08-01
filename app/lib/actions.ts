6493b6618b7cd5fd1e7e86ea191e2f13bed55a6a
Sergio D. Barrera <sdbeng@gmail.com>
2024-08-01 10:22:12 -0700

    add server action to create new invoice, revalidate and redirect back

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,43 @@
 'use server';
 
+import {z} from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+//define a schema that matches the shape of the form object. This schema will
+//validate the formData object before saving it to the database.
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),//coerce from a string to a number while validating its type
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});//omit the id and date fields from the schema
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
-    console.log('rawFormData===', rawFormData);
+    });
+    //it's a good practice to store monetary values in cents in your database to eliminate floating point errors and ensure greater accuracy.
+    const amountInCents =  amount * 100;//basic conversion to cents
+    // const amountInCents = Math.round(amount * 100);//alt:converted and rounded to nearest cent
+
+    //create new data with format "yyyy-mm-dd" for the invoice's creation date
+    const date = new Date().toISOString().split('T')[0];//get the current date in the format "yyyy-mm-dd" from the ISO string then split it at the 'T' character
+
+    //create a SQL query to insert the new invoice into the database and pass in the variables
+    await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;//insert the new invoice into the database
+
+    // const result = await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date}) RETURNING *`;//alt:use the RETURNING clause to return the newly created invoice
+
+    //since i'm updating the data displayed in the invoices route, i want to clear this cache and trigger a new request to the server - do this with the revalidatePath function
+    revalidatePath('/dashboard/invoices');//clear the cache for the invoices route, once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server!!
+
+    redirect('/dashboard/invoices');//redirect the user back to the invoices route after the invoice has been created
 }

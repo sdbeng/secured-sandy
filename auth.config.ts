d06bb3192f46ba3585dd7b1b5be7c690a3f2c511
Sergio D. Barrera <sdbeng@gmail.com>
2024-08-02 18:54:11 -0700

    add authentication-wip

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+        authorized({ auth, request: {nextUrl}}) {
+            //it returns true if user is logged in, false if user is not logged in
+            const isLoggedIn = !!auth?.user;// !! converts the value to a boolean
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if(isOnDashboard) {
+                if(isLoggedIn) return true;
+                return false; //Redirect unauthenticated users to login page
+            }else if(isLoggedIn) {
+                ////Redirect authenticated users to dashboard
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [],//add providers with empty array for now
+} satisfies NextAuthConfig;
---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+export default NextAuth(authConfig).auth;
+
+/* Here you're initializing NextAuth.js with the authConfig object and exporting the auth property. You're also using the matcher option from Middleware to specify that it should run on specific paths.
+The advantage of employing Middleware for this task is that the protected routes will not even start rendering until the Middleware verifies the authentication, enhancing both the security and performance of your application.*/
+
+export const config = {
+    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};

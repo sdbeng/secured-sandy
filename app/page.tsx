8e0596dbd1ede6e11eddc1ad33bbb5612eaf8f15
Sergio D. Barrera <sdbeng@gmail.com>
2024-08-19 08:33:26 -0700

    Update welcome text

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to my site in construction.</strong> 
+            <strong>Hi Guest! Welcome to my site.</strong> 
           </p>
           <Link
             href="/login"

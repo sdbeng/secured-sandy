c0970692667908aa5617546a052243bd7a4420e1
Sergio D. Barrera <sdbeng@gmail.com>
2024-08-19 22:22:18 -0700

    update customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-    return <p>customers page</p>
+    return (
+        <div className="mt-2 text-center ">
+            <h1 className="text-2xl py-2 ">Customers List</h1>
+            
+            <div className="flex justify-center">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Add Customer
+                </button>
+            </div>
+        </div>
+    )
 }
